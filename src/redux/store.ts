a06f5448d15950ed652cdce6d10553dedfd489a0
rayhankobir-dev <rayhankobir793@gmail.com>
2024-02-21 11:49:58 +0600

    fix types error

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalSlice from "./slices/modal.slide";
-import { api } from "./features/api.slice";
 
 const reducer = {
   modal: modalSlice,
-  [api.reducerPath]: api.reducer,
 };
 
 const store = configureStore({
   reducer: reducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
 });
 
 export default store;
